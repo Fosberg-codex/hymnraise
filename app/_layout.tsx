27b0089a34e95b065d581cfa9bd7bfe5debe60dd
Fosberg Addai <83887297+Fosberg-codex@users.noreply.github.com>
2024-08-18 03:11:49 +1200

    setup nativewind to start using tailwind css in expo

---
+++ app/_layout.tsx
@@ -0,0 +1,20 @@
+
+import { Slot } from 'expo-router';
+import '../global.css';
+
+export default function RootLayout() {
+  return <Slot />;
+
+}
+
+
+
+// import { Stack } from 'expo-router';
+// // Import your global CSS file
+// import "../global.css"
+
+// export default function RootLayout() {
+//   return <Stack />;
+// }
+
+
--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
-export default function componentName() {
+export default function HomePage() {
   return (
-    <View>
-      <Text>The new beginning is here</Text>
+    <View className='flex-1 justify-center items-center'>
+      <Text className='bg-pink-600 text-white text-xl px-2'>The new beginning is here</Text>
      </View>
   );
 }
