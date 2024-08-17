8c83823ecb10ae49cf8d26f701bb6e6c449945f5
Fosberg Addai <83887297+Fosberg-codex@users.noreply.github.com>
2024-08-17 22:49:05 +1200

    started the index page

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function componentName() {
+  return (
+    <View>
+      <Text>The new beginning is here</Text>
+     </View>
+  );
+}
